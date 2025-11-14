72c0c71943dd491028b99f70501bfa74edd13c45
Emma <emma.pernin1@gmail.com>
2025-11-14 16:39:30 +0100

    Modal V2

--- js/projets.js
+++ js/projets.js
@@ -25,3 +25,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
     filterProjects('all')
 });
+
+const btns = document.querySelectorAll('[data-modal-target]');
+console.log(btns)
+const closeBtns = document.querySelectorAll('.close-popup');
+const overlay = document.querySelector('.modal-overlay');
+
+const openModal = (modal) => {
+    if (modal == null) return
+    modal.classList.add('open')
+    overlay.classList.add('open')
+}
+
+const closeModal = (modal) => {
+    if (modal == null) return
+    modal.classList.remove('open')
+    overlay.classList.remove('open')
+}
+
+btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const modal = document.querySelector(btn.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+closeBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const modal = btn.closest('.modal')
+        closeModal(modal)
+    })
+})
